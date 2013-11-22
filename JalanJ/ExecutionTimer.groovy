6603765f976a5d1ad34c4f432ea931bac7c306ea
Adrian McMenamin <adrianmcmenamin@gmail.com>
2013-11-22 16:17:22 +0000

    moving towards timing execution model

---
+++ JalanJ/ExecutionTimer.groovy
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package JalanJ
+
+/**
+ * @author adrian
+ *
+ */
+class ExecutionTimer {
+	
+	def controlFile
+	
+	ExecutionTimer(def fileStr)
+	{
+		controlFile = fileStr
+	}
+	
+	def timeExecution()
+	{
+		
+		
+		return 0
+	}
+
+}
