828c98019c5eb521ea742405bb4a0ea0be96667d
Adrian McMenamin <adrianmcmenamin@gmail.com>
2013-12-21 18:16:32 +0000

    Working on LRU/2 allocator

---
+++ jalanJ/LRU2Allocator.groovy
@@ -0,0 +1,89 @@
+/**
+ * 
+ */
+package jalanJ
+
+/**
+ * @author adrian
+ *
+ */
+class LRU2Allocator implements PagingAllocator {
+
+	def lowPriority = [:]
+	def highPriority = [:]
+	def memorySize
+	def highSize
+	def lowSize
+	def totalPages
+	def PAGESHIFT
+	
+	
+	LRU2Allocator(def pageOff, def memSize)
+	{
+		setPageOffset(pageOff)
+		setMaxMemory(memSize)
+	}
+	
+	/* (non-Javadoc)
+	 * @see jalanJ.PagingAllocator#havePage(long)
+	 */
+	@Override
+	public boolean havePage(long address) {
+		if (lowPriority[address >> PAGESHIFT] ||
+			highPriority[address >> PAGESHIFT])
+			return true
+		else return false
+	}
+
+	void reallocatePage(def page)
+	{
+		lowPriority.remove(page)
+		highPriority[page] = new Date()
+		if (highPriority.size() > highSize)
+		{
+			def oldPage = highPriority.min{it.value}
+			def oldDate = highPriority.remove(oldPage)
+			lowPriority[oldPage] = oldDate
+			println "pushed down $oldPage with $oldDate"
+		}
+	}
+	/* (non-Javadoc)
+	 * @see jalanJ.PagingAllocator#allocatePage(long)
+	 */
+	@Override
+	synchronized public boolean allocatePage(long address) {
+		def page = address >> PAGESHIFT
+		if (lowPriority[page]) {
+			reallocatePage(page)
+		} else if (highPriority[page]) {
+			highPriority[page] = new Date()
+			return true
+		} else
+			lowPriority[page] = new Date()
+		if (lowPriorty.size() > lowSize) {
+			lowPriority.remove(lowPriority.min{it.value})
+		}
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see jalanJ.PagingAllocator#setPageOffset(int)
+	 */
+	@Override
+	public void setPageOffset(int bits) {
+		PAGESHIFT = bits
+	}
+
+	/* (non-Javadoc)
+	 * @see jalanJ.PagingAllocator#setMaxMemory(long)
+	 */
+	@Override
+	public void setMaxMemory(long bytes) {
+		memorySize = bytes
+		//low takes 75%, high 25%
+		totalPages = bytes/(1 << PAGESHIFT)
+		highSize = (totalPages * 0.25) as Integer
+		lowSize = totalPages - highSize
+	}
+
+}
