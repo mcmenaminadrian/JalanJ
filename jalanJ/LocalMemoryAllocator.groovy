0fa37892166ac70b6e961b8686cd15e11767f6cd
Adrian McMenamin <adrianmcmenamin@gmail.com>
2014-01-16 23:13:15 +0000

    working on local allocator

---
+++ jalanJ/LocalMemoryAllocator.groovy
@@ -0,0 +1,29 @@
+package jalanJ
+
+class LocalMemoryAllocator implements PagingAllocator {
+
+	@Override
+	public boolean havePage(long address, Object debug) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean allocatePage(long address, Object debug) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setPageOffset(int bits) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setMaxMemory(long bytes) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
